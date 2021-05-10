be6365e0bd7add9adf28fbe12bcd9fc516263b6c
Manabendra Saharia <msaharia@live.com>
2021-05-10 18:08:22 +0530

    Publishing to Github Pages (publish.sh)

--- publish.sh
+++ publish.sh
@@ -10,3 +10,5 @@ echo "Updating master branch"
 cd docs && git add --all && git commit -m "Publishing to Github Pages (publish.sh)"
 
 git push
+
+scp -r docs/* msaharia@hydrosense.iitd.ac.in:/var/www/hydrosense/https/html/ 
